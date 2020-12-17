994fa23dc7fd36cf4f9b40f2f195402f08a88dc5
JoaoP <contato.joaocomp@gmail.com>
2020-12-17 11:05:11 -0300

    partials changes and post

--- src/controllers/campusController.js
+++ src/controllers/campusController.js
@@ -1,5 +1,28 @@
+import Campus from "../models/campus.model.js";
+
 export default {
   index(req, res) {
     res.render("index.ejs");
+  },
+  get(req, res) {
+    res.render("campus.ejs");
+  },
+  post(req, res) {
+    const nome = req.body.nome;
+    const bairro = req.body.bairro;
+    const cidade = req.body.cidade;
+
+    const campus = new Campus({
+      nome: nome,
+      bairro: bairro,
+      cidade: cidade,
+    });
+
+    campus
+      .save()
+      .then(() => {
+        res.redirect("campus");
+      })
+      .catch((err) => console.error(err));
   },
 };
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,7 @@ import campusController from "./controllers/campusController.js";
 const router = express.Router();
 
 router.get("/", campusController.index);
+router.get("/campus", campusController.get);
+router.post("/campus", campusController.post);
 
 export default router;
