d69a3feafabe9c07b75192d1896b4e672a5be522
JoaoP <contato.joaocomp@gmail.com>
2020-12-17 13:12:34 -0300

    CRUD made for 'professores'

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import campusController from "./controllers/campusController.js";
+import professoresController from "./controllers/professoresController.js";
 
 const router = express.Router();
 
@@ -8,5 +9,10 @@ router.get("/campus", campusController.get);
 router.post("/campus", campusController.post);
 router.get("/campus/delete/:id", campusController.delete);
 router.post("/campus/update/:id", campusController.update);
+
+router.get("/professores", professoresController.get);
+router.post("/professores", professoresController.post);
+router.get("/professores/delete/:id", professoresController.delete);
+router.post("/professores/update/:id", professoresController.update);
 
 export default router;
