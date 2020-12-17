965b1061c72b7026d0f84e751ebaccfbc74af43b
Wilton Oliveira <oliveirajrwilton@gmail.com>
2020-12-17 17:10:20 -0300

    Laboratorio

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 import express from "express";
 import campusController from "./controllers/campusController.js";
 
+import laboratorioController from "./controllers/laboratorioController.js";
+
 const router = express.Router();
 
 router.get("/", campusController.index);
+
+router.get("/laboratorio", laboratorioController.get);
+router.post("/laboratorio", laboratorioController.post);
+router.get("/laboratorio/delete/:id", laboratorioController.delete);
+router.post("/laboratorio/update/:id", laboratorioController.update);
 
 export default router;
